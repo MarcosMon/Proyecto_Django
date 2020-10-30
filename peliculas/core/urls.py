566d95db22f83d80c53d574add56d7841e8995dd
sysmarc13@hotmail.com <sysmarc13@hotmail.com>
2020-10-30 21:30:41 +0100

    rutas home y galeria creadas. Template seccional y base creados.

---
+++ peliculas/core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import home, galeria
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('galeria/', galeria, name="galeria"),
+]
--- peliculas/core/views.py
+++ peliculas/core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+context = { 
+        "data" : ['caratula1','caratula2', 'caratula3', 'caratula4','caratula6','caratula4','caratula4'], 
+    } 
+
+def home(request):
+
+
+    return render(request, 'core/home.html', context)
+
+
+def galeria(request):
+
+
+    return render(request, 'core/galeria.html', context)
